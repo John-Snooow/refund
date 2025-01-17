aacff759a7a4e6af70333088d037061c6063bc6b
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-16 23:05:13 -0300

    formatando o valor padão do real Brasileiro no inpunt

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ const amount = document.getElementById("amount")
 
 //captura o evento de input para formatar o valor
 amount.oninput = () =>{
+    //obtem o valor atual do input e remove os caracters não númericos
     let value = amount.value.replace(/\D/g, "")
 
-    amount.value = value
+    //transformando em centavos(ex: 150/100 = 1,5 que é equivalente a 1,50).
+    value = Number(value) / 100
+
+    //Atualiza o valor do input.
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+    //formata o valor na padrão BRL (Real Brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+    //retorna o valor formatado
+    return value
+
 }
