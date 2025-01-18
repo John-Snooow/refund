36166c0680827d37c62fae81a0d922772ad72ac3
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-17 22:35:47 -0300

    Criando um objeto com os detalhes na nova despesa

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //Seleciona os elementos do formulario.
-
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
 
 //captura o evento de input para formatar o valor
 amount.oninput = () =>{
@@ -22,5 +25,22 @@ function formatCurrencyBRL(value){
     })
     //retorna o valor formatado
     return value
+
+}
+//Captura o evento de submit do formulario para obter os valores
+form.onsubmit = (event) => {
+    //Previne o comportamento padão de recarregar a pagina
+    event.preventDefault()
+
+    //Cria um objeto com os detalhes na nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
 
 }
