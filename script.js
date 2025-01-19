4472e0209be08827a1fdb0f0d942773e41c7e9a7
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-18 22:33:31 -0300

    criando a função dos elementos de despesas no "li"

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
+//Seleciona os elementos da lista
+const expenseList = document.querySelector("ul")
+
 
 //captura o evento de input para formatar o valor
 amount.oninput = () =>{
@@ -41,6 +44,29 @@ form.onsubmit = (event) => {
         amount: amount.value,
         created_at: new Date(),
     }
+    //chama a função que irá adicionar o item na lista.
+    expenseAdd(newExpense)
 
+}
 
+function expenseAdd(newExpense){
+    try{
+       // Cria o elemento de il para adicionar o item (li) na lista (ul). 
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+
+        //Cria o icone da categoria.
+        const expenseIcon = document.createElement("img")
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.category_name)
+
+        //Adiciona as informações no item.
+        expenseItem.append(expenseIcon)
+        //Adiciona o item na lista
+        expenseList.append(expenseItem)
+
+    } catch (erro){
+        alert("Não foi possivel atualizar a lista de despesas.")
+        console.log(error)
+    }
 }
